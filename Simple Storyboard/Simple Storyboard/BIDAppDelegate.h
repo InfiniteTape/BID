0f32d938b2076f83f0d3494c89f869f7857fa322
Brian Rogers <infinitetape@gmail.com>
2012-03-30 17:20:58 -0400

    Storyboard projects

---
+++ Simple Storyboard/Simple Storyboard/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Simple Storyboard
+//
+//  Created by Brian Rogers on 3/30/12.
+//  Copyright (c) 2012 infinitetape.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Simple Storyboard/Simple Storyboard/BIDTaskListController.h
@@ -0,0 +1,13 @@
+//
+//  BIDTaskListController.h
+//  Simple Storyboard
+//
+//  Created by Brian Rogers on 3/30/12.
+//  Copyright (c) 2012 infinitetape.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDTaskListController : UITableViewController
+
+@end
