55328e070ad78e2ef33dee4c5e9d8d8166cba0c0
Brian Rogers <brogers@beckman.com>
2012-05-11 14:36:15 -0400

    Chapter 12, through reading NSUserDefaults

--- AppSettings/AppSettings/BIDMainViewController.m
+++ AppSettings/AppSettings/BIDMainViewController.m
@@ -10,6 +10,34 @@
 
 @implementation BIDMainViewController
 
+@synthesize usernameLabel;
+@synthesize passwordLabel; 
+@synthesize protocolLabel; 
+@synthesize warpDriveLabel; 
+@synthesize warpFactorLabel; 
+@synthesize favoriteTeaLabel; 
+@synthesize favoriteCandyLabel; 
+@synthesize favoriteGameLabel;
+@synthesize favoriteExcuseLabel; 
+@synthesize favoriteSinLabel;
+
+- (void)refreshFields 
+{ 
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults]; 
+    usernameLabel.text = [defaults objectForKey:kUsernameKey]; 
+    passwordLabel.text = [defaults objectForKey:kPasswordKey]; 
+    protocolLabel.text = [defaults objectForKey:kProtocolKey]; 
+    warpDriveLabel.text = [defaults boolForKey:kWarpDriveKey]
+        ? @"Engaged" : @"Disabled"; 
+    warpFactorLabel.text = [[defaults objectForKey:kWarpFactorKey] stringValue]; 
+    favoriteTeaLabel.text = [defaults objectForKey:kFavoriteTeaKey];
+    favoriteCandyLabel.text = [defaults objectForKey:kFavoriteCandyKey]; 
+    favoriteGameLabel.text = [defaults objectForKey:kFavoriteGameKey]; 
+    favoriteExcuseLabel.text = [defaults objectForKey:kFavoriteExcuseKey]; 
+    favoriteSinLabel.text = [defaults objectForKey:kFavoriteSinKey];
+}
+    
+    
 
 - (void)didReceiveMemoryWarning
 {
@@ -23,6 +51,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self refreshFields];
 }
 
 - (void)viewDidUnload
@@ -30,6 +59,7 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.usernameLabel = nil; self.passwordLabel = nil; self.protocolLabel = nil; self.warpDriveLabel = nil; self.warpFactorLabel = nil; self.favoriteTeaLabel = nil; self.favoriteCandyLabel = nil; self.favoriteGameLabel = nil; self.favoriteExcuseLabel = nil; self.favoriteSinLabel = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -62,7 +92,9 @@
 
 - (void)flipsideViewControllerDidFinish:(BIDFlipsideViewController *)controller
 {
+    [self refreshFields];
     [self dismissModalViewControllerAnimated:YES];
+
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
